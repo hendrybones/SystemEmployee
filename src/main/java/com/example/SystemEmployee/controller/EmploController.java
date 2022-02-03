1c2f7aadbb3ed1c9a338a5e8c577691c9277b699
bones <hendrymwamburi56@gmail.com>
2022-02-03 23:40:54 +0300

    mapping

--- src/main/java/com/example/SystemEmployee/controller/EmploController.java
+++ src/main/java/com/example/SystemEmployee/controller/EmploController.java
@@ -11,29 +11,29 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-public class EmployeeController {
+public class EmploController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/")
+    @GetMapping("/{id}")
     public String viewHomePage(Model model){
         List<Employee> listEmployee=employeeService.listAll();
         model.addAttribute("listEmployee",listEmployee);
-        System.out.print("Get/ ");
+        System.out.print("Get/{id}");
         return "index";
     }
-    @GetMapping("new")
+    @GetMapping("new{id}")
     public String add(Model model){
         model.addAttribute("Employee", new Employee());
         return "new";
     }
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @RequestMapping(value = "/save{id}", method = RequestMethod.POST)
     public String saveEmployee(@ModelAttribute("employee") Employee employee){
         employeeService.save(employee);
         return "redirect/";
     }
-    @RequestMapping("/edit/{id}")
-    public ModelAndView showEditEmployeePage(@PathVariable(name="id") Long id){
+    @RequestMapping("/edit{id}")
+    public ModelAndView showEditEmployeePage(@PathVariable(name = "id") Long id){
         ModelAndView mav=new ModelAndView("new");
         Employee employee=employeeService.get(id);
         return mav;
